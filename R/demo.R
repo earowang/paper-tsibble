a069cbbc319cea52e699aa6c98e72446f651e9c7
earowang <earo.wang@gmail.com>
2018-06-14 15:02:47 +1000

    added explanations to tb data

--- R/demo.R
+++ R/demo.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(tsibble)
 
-## ---- load-data
+## ---- tb-sub
 tb_small <- read_rds("data/tb_small.rds")
 tb_small %>% 
   knitr::kable(booktabs = TRUE, caption = "(ref:tb-sub)", linesep = "") %>% 
