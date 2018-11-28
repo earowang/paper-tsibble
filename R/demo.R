0bb0f4130caec41f6185418466e6c7edbe134b5b
earowang <earo.wang@gmail.com>
2018-11-28 11:53:07 +1100

    added "data first" subsection

--- R/demo.R
+++ R/demo.R
@@ -7,3 +7,7 @@ tb_small <- read_rds("data/tb-small.rds")
 tb_small %>% 
   knitr::kable(booktabs = TRUE, caption = "(ref:tb-sub)", linesep = "") %>% 
   kableExtra::kable_styling(position = "center")
+
+## ---- tb-print
+as_tsibble(tb_small, key = id(country, gender), index = year) %>% 
+  print(n = 5)
